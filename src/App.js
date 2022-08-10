d14174e6fb6fb8c1878e8bfb29f312a77fe771d9
Matt2017 <mjordan1616@gmail.com>
2022-08-09 21:14:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import React, { Component } from "react"
+import React from "react"
+import ReactDOM from "react-dom"
 import logo from "./logo.svg"
 import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>Testing PV</h2>
-        <ul>
-        <li>one</li>
-        <li>one</li>
-        </ul>
-      </div>
-    )
-  }
-}
 function apptwo() {
   return(
       <div className="App">
@@ -55,5 +15,5 @@ function apptwo() {
       </div>
   )
 }
-export default App
+
 ReactDOM.render(<apptwo />, document.getElementById('root'))
